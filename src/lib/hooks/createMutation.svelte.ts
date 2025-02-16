14437a82d99ab31cf0b9e251e4f3792954699b62
attaryu <mattarannaufal@gmail.com>
2025-02-16 18:02:56 +0700

    feat: add adapter for store-based tanstack-query to runes reactivity

---
+++ src/lib/hooks/createMutation.svelte.ts
@@ -0,0 +1,61 @@
+import type { CreateBaseMutationResult, CreateMutationOptions } from '@tanstack/svelte-query';
+
+import type { IErrorResponseAPI } from '$lib/types/response';
+
+import { createMutation as TanstackCreateMutation } from '@tanstack/svelte-query';
+import { get } from 'svelte/store';
+
+export function createMutation<Response, Payload>(
+	options: CreateMutationOptions<Response | undefined, IErrorResponseAPI, Payload, unknown>,
+) {
+	/**
+	 * original mutation store from tanstack/svelte-query
+	 */
+	const mutationStore = TanstackCreateMutation(options);
+
+	/**
+	 * mutation state value for easy reactivity
+	 */
+	let mutation = $state.raw<
+		CreateBaseMutationResult<Response | undefined, IErrorResponseAPI, Payload, unknown>
+	>(
+		// get mutation store value immediately for avoid undefined value
+		get(mutationStore),
+	);
+
+	$effect(() => {
+		const unsubscribe = mutationStore.subscribe((state) => {
+			// update mutation state value
+			mutation = state;
+		});
+
+		return () => {
+			unsubscribe();
+		};
+	});
+
+	return {
+		get data() {
+			return mutation.data;
+		},
+
+		get error() {
+			return mutation.error;
+		},
+
+		get isSuccess() {
+			return mutation.isSuccess;
+		},
+
+		get isError() {
+			return mutation.isError;
+		},
+
+		get isPending() {
+			return mutation.isPending;
+		},
+
+		mutate: mutation.mutate,
+		mutateAsync: mutation.mutateAsync,
+	};
+}
---
+++ src/lib/hooks/createQuery.svelte.ts
@@ -0,0 +1,66 @@
+import type { IErrorResponseAPI } from '$lib/types/response';
+import type { QueryObserverResult, UndefinedInitialDataOptions } from '@tanstack/svelte-query';
+
+import { createQuery as TanstackCreateQuery } from '@tanstack/svelte-query';
+import { get } from 'svelte/store';
+
+export function createQuery<Response>(
+	options: UndefinedInitialDataOptions<
+		Response | undefined,
+		IErrorResponseAPI,
+		Response | undefined,
+		string[]
+	>,
+) {
+	/**
+	 * original query store from tanstack/svelte-query
+	 */
+	const queryStore = TanstackCreateQuery(options);
+
+	/**
+	 * query state value for easy reactivity
+	 */
+	let query = $state.raw<QueryObserverResult<Response | undefined, IErrorResponseAPI>>(
+		// get query store value immediately for avoid undefined value
+		get(queryStore),
+	);
+
+	$effect(() => {
+		const unsubscribe = queryStore.subscribe((state) => {
+			// update query state value
+			query = state;
+		});
+
+		return () => {
+			unsubscribe();
+		};
+	});
+
+	return {
+		get data() {
+			return query.data;
+		},
+
+		get error() {
+			return query.error;
+		},
+
+		get isSuccess() {
+			return query.isSuccess;
+		},
+
+		get isError() {
+			return query.isError;
+		},
+
+		get isPending() {
+			return query.isPending;
+		},
+
+		get isLoading() {
+			return query.isLoading;
+		},
+
+		refetch: query.refetch,
+	};
+}
