42fafa91ccd24b599a63a1c6f86435cd2c096ad1
attaryu <mattarannaufal@gmail.com>
2025-03-14 21:18:41 +0700

    refactor: simplify Searchbar component

--- src/lib/components/Searchbar.svelte
+++ src/lib/components/Searchbar.svelte
@@ -1,48 +1,18 @@
 <script lang="ts">
-	import { replaceState } from '$app/navigation';
-	import { page } from '$app/stores';
 	import { Search } from 'lucide-svelte';
-	import { SvelteURLSearchParams } from 'svelte/reactivity';
 
 	import Input from './Input.svelte';
 
-	type Props = {
+	interface Props {
 		class?: string;
 		placeholder?: string;
 		value?: string;
-	};
+	}
 
 	let { class: className, value = $bindable(), placeholder = 'Search' }: Props = $props();
-
-	const KEY = 'search';
-	const searchParams = new SvelteURLSearchParams($page.url.search);
-
-	function replaceSearchParamsOnURL() {
-		if (searchParams.has(KEY)) {
-			replaceState(`?${searchParams.toString()}`, {});
-		} else {
-			replaceState($page.url.pathname, {});
-		}
-	}
-
-	function onChangeHandler(event: EventParameter<Event, HTMLInputElement>) {
-		const { value } = event.currentTarget;
-
-		if (value) {
-			searchParams.set(KEY, value);
-		} else {
-			searchParams.delete(KEY);
-		}
-
-		replaceSearchParamsOnURL();
-	}
-
-	$effect(() => {
-		value = searchParams.get(KEY) ?? '';
-	});
 </script>
 
-<Input {placeholder} type="text" class={className} {value} oninput={onChangeHandler}>
+<Input {placeholder} type="text" class={className} bind:value>
 	{#snippet icon(props)}
 		<Search {...props} />
 	{/snippet}
