35431bc258d19fbb862d2078775701fcd439c5e7
attaryu <mattarannaufal@gmail.com>
2025-03-26 21:19:03 +0700

    refactor: remove deprecated children prop from FieldError component

--- src/lib/components/FieldError.svelte
+++ src/lib/components/FieldError.svelte
@@ -5,22 +5,16 @@
 	import Text from './Text.svelte';
 
 	interface Props {
-		children?: Snippet;
 		class?: string;
 		message?: string | string[] | null;
 	}
 
-	const { children, message, class: className }: Props = $props();
+	const { message, class: className }: Props = $props();
 
 	const _message = $derived(Array.isArray(message) ? message[0] : message);
 </script>
 
 {#if _message}
-	<Text tag="small" class={mergeClass('text-center text-red-500', className)}>
-		{_message}
-	</Text>
-{:else if children}
-	<!-- deprecated, use message property instead -->
 	<Text tag="small" class={mergeClass('text-center text-red-500', className)}>
 		{_message}
 	</Text>
