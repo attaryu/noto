6502cb9f5541dc1ebd3b1e8b88f370ad76a8d102
attaryu <mattarannaufal@gmail.com>
2025-04-16 12:21:28 +0700

    fix: update selected item handling in Select component

--- src/lib/components/Select.svelte
+++ src/lib/components/Select.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
-	
+	import { m } from 'paraglide/messages';
+
 	import DropdownBase from './DropdownBase.svelte';
 
 	interface Item {
@@ -12,24 +12,24 @@
 		items: Array<Item>;
 		class?: string;
 		placeholder?: string;
-		selected?: Item;
+		selected?: Item['value'];
 	};
 
-	const { class: className, items, placeholder, selected }: Props = $props();
+	let { class: className, items, placeholder, selected = $bindable() }: Props = $props();
 
-	let selectedItem: Item | undefined = $state(selected);
+	const selectedTitle = $derived(items.find((item) => item.value === selected)?.title);
 </script>
 
 <DropdownBase
 	class={className}
 	{items}
-	display={selectedItem?.title ?? placeholder ?? 'Select an item'}
+	display={selectedTitle ?? placeholder ?? m['select_component.default_display']()}
 >
 	{#snippet itemRender(className, item)}
 		<button
 			class={className}
-			disabled={selectedItem?.value === item.value}
-			onclick={() => (selectedItem = item)}>{item.title}</button
+			disabled={selected === item.value}
+			onclick={() => (selected = item.value)}>{item.title}</button
 		>
 	{/snippet}
 </DropdownBase>
